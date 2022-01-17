402971cf1916981680371744f64f7dc5df5dac36
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2022-01-17 21:26:11 +0530

    Project Complete

--- server.js
+++ server.js
@@ -38,6 +38,16 @@ app.patch("/deleteNotes", verify, deleteNotes);
 app.post("/signup", signup);
 app.post("/login", login);
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const port = 6000;
 
